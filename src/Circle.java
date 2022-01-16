6e28cb4f2f4e1c6a432c4b3db30993e93b3fa7cf
Woody <mmj2rv9@gmail.com>
2022-01-16 13:17:33 +0900

    입력 값에 따른 원 그리기

---
+++ src/Circle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Circle {
+
+    // 원의 방정식을 이용
+    // n을 중심으로 n * n 보다 작으면 별찍고 크면 공백
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("n 값 입력");
+        int n = input.nextInt();
+
+        for (int i = -n; i <= n; i++) {
+
+            for (int j = -n; j <= n; j++) {
+
+                if ((i * i) + (j * j) <= n * n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
