0430a2b23cb143658cc7b06bcf5e6189ddcd8afd
Woody <mmj2rv9@gmail.com>
2022-01-21 15:33:24 +0900

    StopWatch 구현

---
+++ src/StopWatch.java
@@ -0,0 +1,188 @@
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StopWatch {
+
+    public static void main(String[] args) {
+
+        String[] hours = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+        String[] minutes = {"5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"};
+        String[] seconds = {"5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"};
+
+        Timer timer = new Timer();
+
+        final int[] hour = new int[1];
+
+        final int[] minute = new int[1];
+
+        final int[] second = new int[1];
+
+        JFrame fr = new JFrame();
+        JPanel pnl = new JPanel();
+
+        // Hour
+        JComboBox hoursBox = new JComboBox(hours);
+        JScrollPane hoursScrollPnl = new JScrollPane(hoursBox);
+
+        // Minute
+        JComboBox minuteBox = new JComboBox(minutes);
+        JScrollPane minuteScrollPnl = new JScrollPane(minuteBox);
+
+        // Seconds
+        JComboBox secondBox = new JComboBox(seconds);
+        JScrollPane secondScrollPnl = new JScrollPane(secondBox);
+
+        // comboBox에서 선택하여 시간 가져오기
+        hoursBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = hoursBox.getSelectedItem().toString();
+                hour[0] = Integer.parseInt(number);
+            }
+        });
+
+        minuteBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = minuteBox.getSelectedItem().toString();
+                minute[0] = Integer.parseInt(number);
+            }
+        });
+
+        secondBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String number = secondBox.getSelectedItem().toString();
+                second[0] = Integer.parseInt(number);
+            }
+        });
+
+
+        JLabel jLabel = new JLabel();
+        JLabel jLabel1 = new JLabel();
+        JLabel jLabel2 = new JLabel();
+        JLabel jLabel3 = new JLabel();
+
+        JLabel text = new JLabel("시");
+        JLabel text2 = new JLabel("분");
+        JLabel text3 = new JLabel("초");
+
+        JButton jBtn1 = new JButton("START");
+        JButton jBtn2 = new JButton("RESET");
+        JButton jBtn3 = new JButton("STOP");
+
+        jBtn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton jb = (JButton)e.getSource();
+                if (jb.getText().equals("START")) {
+                    TimerTask task = new TimerTask() {
+
+                        int h = hour[0];
+                        int m = minute[0];
+                        int s = second[0];
+
+                        @Override
+                        public void run() {
+                            if (h == 0 && m == 0 && s == 0) {
+                                timer.cancel();
+                            }
+
+                            if (h != 0 && m == 0) {
+                                m = 60;
+                            }
+
+                            if (m != 0 && s == 0) {
+                                s = 60;
+                            }
+
+                            s--;
+
+                            if (s == 0) {
+                                m--;
+                            }
+
+                            if (m == 0) {
+                                h--;
+                            }
+
+                            jLabel.setText("  타이머가 시작됩니다.");
+                            jLabel1.setText(h + "" + "시");
+                            jLabel2.setText(m + "" + "분");
+                            jLabel3.setText(s + "" + "초");
+                        }
+                    };
+                    timer.schedule(task, 0, 1000);
+                }
+            }
+        });
+
+        jBtn2.addActionListener(new ActionListener() {
+
+            int h = 0;
+            int m = 0;
+            int s = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton jb = (JButton) e.getSource();
+                if (jb.getText().equals("RESET")) {
+                    jLabel.setText("타이머가 리셋됩니다.");
+                    jLabel1.setText(h + "" + "시");
+                    jLabel2.setText(m + "" + "분");
+                    jLabel3.setText(s + "" + "초");
+                }
+            }
+        });
+
+
+        jBtn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton jb = (JButton) e.getSource();
+                if (jb.getText().equals("STOP")) {
+                    jLabel.setText("타이머가 멈춥니다.");
+                    timer.cancel();
+                }
+            }
+        });
+
+        // 시간 선택
+        // Hour
+        pnl.add(hoursScrollPnl);
+        pnl.add(text);
+
+        // Minute
+        pnl.add(minuteScrollPnl);
+        pnl.add(text2);
+
+        // Seconds
+        pnl.add(secondScrollPnl);
+        pnl.add(text3);
+
+        // Btn
+        pnl.add(jBtn1);
+        pnl.add(jBtn2);
+        pnl.add(jBtn3);
+
+        // Text
+        pnl.add(jLabel);
+
+        pnl.add(jLabel1);
+        pnl.add(jLabel2);
+        pnl.add(jLabel3);
+
+        fr.add(pnl);
+
+        fr.setTitle("스톱워치");
+        fr.setSize(300, 300);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setVisible(true);
+
+    }
+
+}
